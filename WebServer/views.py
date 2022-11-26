860bbc4f09808cdd9664ede5ea6e9fdc8e6e6319
Jheison Leon <201905980@est.umss.edu>
2022-11-25 23:43:35 -0400

    index

--- WebServer/views.py
+++ WebServer/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    """if request.method == 'GET':
+        if not request.user.is_anonymous:
+            if request.user.email.endswith('@delegacion.com'):
+                return redirect('delegacionTorneo')
+            elif request.user.email.endswith('@admin.com'):
+                return redirect('torneos')
+        else:
+            torneosProgreso = Torneo.objects.filter(torneo_estado=1)
+            aux = []
+            for i in range(len(torneosProgreso)):
+                aux.append(i+1)
+            return render(request, 'index.html', {
+                "torneos": torneosProgreso,
+                "longitud": aux
+            })"""
+    if request.method == 'GET':
+        return render(request, 'index.html')
